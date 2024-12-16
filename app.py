d451aeaa37a885679517f9d923f7452e1c71ee2f
Mykola Purdes <112871036+NickPurdes@users.noreply.github.com>
2024-12-16 14:05:44 +0200

    col renamed

--- app.py
+++ app.py
@@ -51,9 +51,9 @@ def get_weather():
     hourly_data["relative_humidity_2m"] = hourly_relative_humidity_2m
 
     hourly_dataframe = pd.DataFrame(data = hourly_data, index=None)
-    hourly_dataframe.columns['temp', 'humidity']
+    df = hourly_dataframe.rename(columns={"temperature_2m":"temp", "relative_humidity_2m":"humidity"})
 
-    df_styled = hourly_dataframe.iloc[-10:].style\
+    df_styled = df.iloc[-10:].style\
         .format(precision=1)\
         .highlight_max(color='yellowgreen', subset=["temperature_2m"])\
         .highlight_min(color='coral', subset=["temperature_2m"])
