22f2205d9550db0a498fcc4001dbca3b1f3f0844
Mykola Purdes <112871036+NickPurdes@users.noreply.github.com>
2024-12-17 11:14:46 +0200

    formatting

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def get_weather():
 
     df_styled = hourly_dataframe.iloc[-12:].style\
         .set_caption("Погода на сьогодні")\
-        .format(precision=1, subset='temp')\
+        .format({0: '{:.2f}', 1: '{:.1f} C'}, precision=2)\
         .hide(axis='index')      
         
     return df_styled.to_string(sparse_index=True, delimiter='|', sparse_columns=True)
